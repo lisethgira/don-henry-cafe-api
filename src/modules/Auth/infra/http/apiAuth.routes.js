20584d5e9747891a6d322b9550a1ef7e9b9f635c
Liseth Giraldo <liseth.giraldo@rocketfy.co>
2023-10-16 01:21:55 -0500

    feat: se modifica enrutamiento

--- src/modules/Auth/infra/http/apiAuth.routes.js
+++ src/modules/Auth/infra/http/apiAuth.routes.js
@@ -7,31 +7,5 @@ routes.post("/login", async (req, res)=>{
     const controllerMain = new classController()
     await controllerMain.Login(req, res)
 })
-
-
-routes.get("/", (req, res) => {
-    res.status(200).json({
-      status: 200,
-      msg: "Welcome to Rest API Don Henry Café app!",
-      contributors: [
-        {
-          alias: "lisethgira",
-          github: "https://github.com/lisethgira",
-        },
-        {
-          alias: "Josed1804",
-          github: "https://github.com/Josed1804",
-        },
-        {
-          alias: "ESCUDERO457",
-          github: "https://github.com/ESCUDERO457",
-        },
-        {
-          alias: "frankc2812",
-          github: "https://github.com/frankc2812",
-        },
-      ],
-    });
-  });
   
 module.exports = routes
--- src/modules/Main/app/index.js
+++ src/modules/Main/app/index.js
@@ -1,15 +1,18 @@
 //Librerias
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 
 //Librerias adicionales
-const cors =  require("cors")
-const morgan = require("morgan")
+const cors =  require("cors");
+const morgan = require("morgan");
 const helmet = require("helmet");
 const compression = require("compression");
 
 //Declaracion de variables globales
 app.set("port", process.env.APP_PORT);
+
+// routes
+const mainRoutes = require("../infra/http/apiMain.routes");
 
 //Middlewares
 app.use(cors())
@@ -18,10 +21,10 @@ app.use(morgan((process.env.ENV = "development" ? "dev" : "common")))
 app.use(helmet());
 app.use(compression({ level: 9 }));
 
-//Static
-
-
 //Router
-app.use(require("../../Auth/infra/http/apiAuth.routes"))
+app.use("/henrycafe/api", mainRoutes);
+
+//Static
+app.use(express.static("public"));
 
 module.exports = app
